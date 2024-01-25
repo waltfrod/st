ed2f8044f530aa9ce96204156d4d91025628e047
Walter Fabián Rodríguez Salazar <wfabianrs@gmail.com>
2024-01-25 16:59:36 -0500

    :construction: [upd] Parche: Default font size

--- x.c
+++ x.c
@@ -4,6 +4,7 @@
 #include <limits.h>
 #include <locale.h>
 #include <signal.h>
+#include <stdlib.h>
 #include <sys/select.h>
 #include <time.h>
 #include <unistd.h>
@@ -1145,7 +1146,7 @@ xinit(int cols, int rows)
 		die("could not init fontconfig.\n");
 
 	usedfont = (opt_font == NULL)? font : opt_font;
-	xloadfonts(usedfont, 0);
+	xloadfonts(usedfont, defaultfontsize);
 
 	/* colors */
 	xw.cmap = XDefaultColormap(xw.dpy, xw.scr);
@@ -2067,6 +2068,11 @@ main(int argc, char *argv[])
 	case 'v':
 		die("%s " VERSION "\n", argv0);
 		break;
+    case 'z':
+        defaultfontsize = strtod(EARGF(usage()), NULL);
+        if (!(defaultfontsize > 0))
+            usage();
+        break;
 	default:
 		usage();
 	} ARGEND;
